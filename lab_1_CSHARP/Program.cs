0a2c5f7d4783b996005e5510039e7111ac92deaa
alexsudochakov37@gmail.com <alexsudochakov37@gmail.com>
2020-09-29 12:45:24 +0300

    Добавьте файлы проекта.

---
+++ lab_1_CSHARP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Введите число № 1:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число № 2:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Metod(num1, num2);
+        }
+
+        static void Metod(int a, int b)
+        {
+            Console.WriteLine($"Произведение {a} на {b}, равно = {a * b}, а его квадрат = {Math.Pow(a * b, 2)}");        
+        }
+    }
+}
